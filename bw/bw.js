5be9e8b592de30ba61ad2fcc2f3dce0d961b7873
Timothy Wojtylak <t.wojtylak@gmail.com>
2015-12-09 12:38:39 -0500

    Official v1.0

--- bw/bw.js
+++ bw/bw.js
@@ -6,19 +6,52 @@
  * 
  */
 ;(function ($) {
-	var canNum = 0;
-	$.fn.bw = function () {
-		$(this).find('*').each(function(){
-			var bgcolor = $(this).css([
+	var list = new Array();
+	$(document).ready(function(){
+		$('body').find('*').each(function(){
+			list.push(new getDOM($(this)));	
+		});
+	});
+
+	var methods = {
+
+		bright: function(el)
+		{
+			var index = 0;
+			for(var i=0; i < list.length; i++)
+			{
+
+				if (list[i].name.context == el.context)
+				{
+					index = i+1;
+				}
+			}
+			for(var i=index; i < list.length; i++)
+			{
+				for (var property in list[i].color)
+				{
+					if (list[i].color.hasOwnProperty(property)) {
+						list[i].name.css(property, list[i].color[property]);
+
+					}
+				}
+			}	
+		},
+
+		dull: function(el)
+		{
+			el.find('*').each(function(){
+			var me = $(this);
+			var bgcolor = me.css([
 				"background-color", 
 				"color",
 				"border-top-color",
 				"border-right-color",
 				"border-bottom-color",
 				"border-left-color"]);
-			var element = $(this);
-
+				
 			$.each(bgcolor, function(prop, value) {
+				
 				var check = value.split("(");
 				if(check[0] == "rgb")
 				{
@@ -29,14 +62,34 @@
 					tim[0] = tim[0].split("(").pop();
 					tim[3] = tim[3].split(")").splice(0,1);
 					var con = convert(tim[0], tim[1], tim[2]);
-					element.css(prop, 'rgba('+con+','+con+','+con+','+tim[3]+')');
-			})
-		});
+					me.css(prop, 'rgba('+con+','+con+','+con+','+tim[3]+')');
+				});
+			});
+		}
+	};
+
+	$.fn.bw = function (options) {
+			return this.each(function () {
+				
+				methods[options].call(this, $(this));
+			});
 	};
 
 	function convert(r, g, b)
 	{
 		var x = Math.ceil((0.299*r) + (0.587*g) + (0.114*b));
 		return x;
+	}
+
+	function getDOM(element)
+	{
+		this.name = element;
+		this.color = element.css([
+			"background-color", 
+			"color",
+			"border-top-color",
+			"border-right-color",
+			"border-bottom-color",
+			"border-left-color"]);
 	}
 })(jQuery);
